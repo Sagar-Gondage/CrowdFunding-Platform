ad7604ca6ac9b6b46211014c9d026b4fd05a0153
Sagar-Gondage <sagargondage370@gmail.com>
2022-12-25 18:47:44 +0530

    donation functionality done

--- client/src/components/DisplayCampaigns.jsx
+++ client/src/components/DisplayCampaigns.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 
 import FundCard from "./FundCard";
 import { loader } from "../assets";
+import Loader from "./Loader";
 
 const DisplayCampaigns = ({ title, isLoading, campaigns }) => {
   const navigate = useNavigate();
@@ -19,13 +20,7 @@ const DisplayCampaigns = ({ title, isLoading, campaigns }) => {
       </h1>
 
       <div className="flex flex-wrap mt-[20px] gap-[26px]">
-        {isLoading && (
-          <img
-            src={loader}
-            alt="loader"
-            className="w-[100px] h-[100px] object-contain"
-          />
-        )}
+        {isLoading && <Loader title={"Loading"} />}
 
         {!isLoading && campaigns.length === 0 && (
           <p className="font-epilogue font-semibold text-[14px] leading-[30px] text-[#818183]">
